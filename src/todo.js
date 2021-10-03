de04a82ad92a436ccaa6e1b95794c2d63468412b
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-10-03 12:02:00 +0900

    late commit - draft condition, initial commit

---
+++ src/todo.js
@@ -0,0 +1,4 @@
+const todo = document.getElementById("todo-form");
+const toDoList = document.getElementById("todo-list");
+
+function on
---
+++ src/weather.js
@@ -0,0 +1,24 @@
+// const API = api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={3acbbe0d3c8690142ba17125de8e23f8}
+const API_KEY = "3acbbe0d3c8690142ba17125de8e23f8"
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    // console.log("You live in", lat, lng);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    // console.log(url);
+    fetch(url).then(response => response.json()).then(data => {
+        // console.log(data.name, data.weather[0].main)
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+    alert("can't find you. No weather for you.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
