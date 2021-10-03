44c57cd55e8a8872fee843f8d26b07df8e8cd5bc
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-10-03 23:31:40 +0900

    minimum requirement accomplished. no much css

--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,20 @@
-const todo = document.getElementById("todo-form");
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newTodo = toDoInput.value; //toDoInput.value(); will only perform it. so, just call 'value' from toDoInput.
+    toDoInput.value = "";
+    paintToDo(newTodo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
